91ba59d1612a8bf2a9dce3adb7c2e4bc52a17d89
1310650320 <13110037505@163.com>
2015-04-19 11:34:21 +0800

    commit

---
+++ main.py
@@ -0,0 +1,8 @@
+﻿
+for i in range(9,0,-1):
+    for k in range(i-1,1):
+        print(end="    ")
+    for j in range(9,0,-1):
+        q=str.format("{0:1}*{1:1}={2:>2}",i,j,i*j)
+        print(q,end="  ")
+    print()
